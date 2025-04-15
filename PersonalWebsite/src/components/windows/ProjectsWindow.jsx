0fe13dca7a5a5b50537aac7cc122d24f6bdbea4b
SandyLlapa <125626998+SandyLlapa@users.noreply.github.com>
2025-04-14 20:07:50 -0500

    Finished with layout of Project Window. Just have to fill in the correct information for each project. Designed the layout for Experience window.

--- PersonalWebsite/src/components/windows/ProjectsWindow.jsx
+++ PersonalWebsite/src/components/windows/ProjectsWindow.jsx
@@ -10,19 +10,76 @@ const ProjectsWindow = () => {
       id: 1,
       title: "Drone Simulation",
       description: "Interactive drone routing with A* and Dijkstra.",
-      image: "/src/assets/Drone.png"
+      image: "/src/assets/Drone.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      codeLink: "https://github.com/SandyLlapa/droneSimulation",
+      liveLink: "https://hub.docker.com/repository/docker/sandyllapa/drone_sim/general"
+
     },
     {
       id: 2,
-      title: "Weather App",
-      description: "Current weather by city using a weather API.",
-      image: "/path/to/image2.jpg"
+      title: "ThoughtSphere",
+      description: "Social media app with image posts and friend management.",
+      image: "/src/assets/ThoughtSphere.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      codeLink:"https://github.com/SandyLlapa/ThoughtSphere"
     },
     {
       id: 3,
-      title: "ThoughtSphere",
+      title: "Task Manager",
       description: "Social media app with image posts and friend management.",
-      image: "/path/to/image3.jpg"
+      image: "/src/assets/Checklist.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      codeLink: "https://github.com/SandyLlapa/TodoList_v2"
+
+    },
+    {
+      id: 4,
+      title: "Command Line Interpreter from Scratch",
+      description: "Social media app with image posts and friend management.",
+      image: "/src/assets/Command_line.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      codeLink:"https://github.com/SandyLlapa/CommandLine-Interpreter"
+    },
+    {
+      id: 5,
+      title: "Weather App",
+      description: "Current weather by city using a weather API.",
+      image: "/src/assets/Weather.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      codeLink: "https://github.com/SandyLlapa/Weather-App"
+    },
+    {
+      id: 6,
+      title: "Virutal Motion Game",
+      description: "Current weather by city using a weather API.",
+      image: "/src/assets/Dancing.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      liveLink: "https://csci-4611-spring-2024.github.io/assignment-4-SandyLlapa/"
+    },
+    {
+      id: 7,
+      title: "Mine Sweeper",
+      description: "Current weather by city using a weather API.",
+      image: "/src/assets/Minesweeper.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      liveLink: "https://csci-4611-spring-2024.github.io/assignment-1-SandyLlapa/"
+    },
+    {
+      id: 8,
+      title: "Earthquake Visualization",
+      description: "Current weather by city using a weather API.",
+      image: "/src/assets/Earth.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      liveLink:"https://csci-4611-spring-2024.github.io/assignment-3-SandyLlapa/"
+    },
+    {
+      id: 9,
+      title: "Ball 'n Hole Game",
+      description: "Current weather by city using a weather API.",
+      image: "/src/assets/Hole.png",
+      tech: ["HTML", "CSS", "JavaScript","C++", "Docker", "Agile Development","UML Design", "Design Patterns" ],
+      liveLink: "https://csci-4611-spring-2024.github.io/assignment-2-SandyLlapa/"
     }
   ];
 
@@ -76,10 +133,31 @@ const ProjectsWindow = () => {
             
 
             <div className="project-header">
-              <h1>{projects[currentIndex].title}</h1>
+              <h2>{projects[currentIndex].title}</h2>
             </div>
             
             <p>{projects[currentIndex].description}</p>
+
+            <div className="tech-stack">
+              {projects[currentIndex].tech.map((item, index) => (
+                <span key={index} className="tech-tag">
+                  {item}
+                </span>
+              ))}
+            </div>
+
+            <div className="project-links">
+              <a href={projects[currentIndex].codeLink} target="_blank" rel="noopener noreferrer" className="link-btn">
+                View Code
+              </a>
+              {projects[currentIndex].liveLink && (
+                <a href={projects[currentIndex].liveLink} target="_blank" rel="noopener noreferrer" className="link-btn">
+                  Live Demo
+                </a>
+              )}
+            </div>
+
+
           </Motion.div>
         </AnimatePresence>
       </div>
